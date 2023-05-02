29eda9e13790041de9d1ac71bb176d790fe12dc7
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-05-01 20:10:05 -0500

    Adding testing

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify, request
+import psycopg2
+import os
+import json
+
+app = Flask(__name__)
+
+# establish connection to database
+conn = psycopg2.connect(
+     host="dpg-ch5bsu1jvhts7knifekg-a.oregon-postgres.render.com",
+     database="mutexdatabase",
+     user="mutexdatabase_user",
+     password="fjIWP0PAHvHQJWAmkOdFMLpiT6CM9X9p"
+)
+
+# create a cursor
+cursor = conn.cursor()
+
+# define route for POST request
+@app.route('/', methods=['POST'])
+def get_data():
+    # get data from request
+    data = request.get_json()
+    column_name = data['column']
+    num_rows = data['rows']
+    
+    # execute SQL query to get column data
+    tabla = "olympic"
+    cursor.execute(f"SELECT {column_name} FROM {tabla} LIMIT {num_rows}")
+
+    # fetch all rows
+    rows = cursor.fetchall()
+
+    # create a list of column data
+    column_data = [row[0] for row in rows]
+
+    # create JSON object
+    json_data = json.dumps(column_data)
+
+    # close cursor and connection
+    cursor.close()
+    conn.close()
+
+    # return JSON data
+    return jsonify(json_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
